2afa22f59f56ff460f02e61b15d46d39745c6ad1
User <maryamkhodadadi.ahz@gmail.com>
2019-05-01 01:12:02 +0800

    R commands to create Plot2

---
+++ Plot2.R
@@ -0,0 +1,12 @@
+
+Dataset <- read.table("household_power_consumption.txt", sep = ";", na.strings = "?",
+                      col.names = names(dataset),header = FALSE, nrows = 2880,
+                      skip = 66637)
+
+library(lubridate)
+Dataset$Datetime <- ymd_hms(paste(Dataset$Date, Dataset$Time))
+
+## Creating Plot 2
+plot(x=Dataset$Datetime, y=Dataset$Global_active_power, type = "l",
+     ylab = "Global Active Power(Kilowatts)")
+
