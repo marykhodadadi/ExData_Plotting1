27def8935aeac55785ddca278c6a70bca3f7892a
User <maryamkhodadadi.ahz@gmail.com>
2019-05-03 01:03:59 +0800

    R command to create plot4

--- Plot4.R
+++ Plot4.R
@@ -5,7 +5,7 @@ Dataset <- read.table("household_power_consumption.txt", sep = ";", na.strings =
                       skip = 66637)
 
 ## Creating Plot 4
-par(mfrow= c(2,2))
+par(mfrow= c(2,2), mar=c(2,3,2,2))
 plot(x= Dataset$Datetime, y= Dataset$Global_active_power ,type = "l",
      xlab = "", ylab = "Global Active Power")
 plot(x= Dataset$Datetime, y= Dataset$Voltage, type = "l",
@@ -16,6 +16,7 @@ points(x=Dataset$Datetime, y= Dataset$Sub_metering_2, type = "l", col= "red")
 points(x=Dataset$Datetime, y= Dataset$Sub_metering_3, type = "l", col= "blue")
 legend("topright",
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       col = c("black", "red", "blue"), lty = 1)
+       col = c("black", "red", "blue"), lwd = 2, bty = 'n', cex = 0.85)
+
 plot(x= Dataset$Datetime, y= Dataset$Global_reactive_power,
      type = "l", xlab = "datetime", ylab = "Global_reactive_power")  
